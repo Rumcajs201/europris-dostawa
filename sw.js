10c72e8d6f79ca0487e0ff30ce7f40a05287c177
Rumcajs201 <95769038+Rumcajs201@users.noreply.github.com>
2026-07-14 13:09:24 +0200

    Add files via upload

--- sw.js
+++ sw.js
@@ -1,1 +1,45 @@
-const C="europris-v5";self.addEventListener("install",e=>{self.skipWaiting()});self.addEventListener("activate",e=>{e.waitUntil(caches.keys().then(k=>Promise.all(k.map(x=>caches.delete(x)))));self.clients.claim()});self.addEventListener("fetch",e=>{if(e.request.method==="GET")e.respondWith(fetch(e.request).catch(()=>caches.match(e.request)))})
+const CACHE_NAME = "europris-app-v6";
+
+const STATIC_FILES = [
+  "./",
+  "./index.html",
+  "./manifest.webmanifest",
+  "./stores.json",
+  "./europris-icon-180.png",
+  "./europris-icon-192.png",
+  "./europris-icon-512.png"
+];
+
+self.addEventListener("install", event => {
+  event.waitUntil(
+    caches.open(CACHE_NAME).then(cache => cache.addAll(STATIC_FILES))
+  );
+  self.skipWaiting();
+});
+
+self.addEventListener("activate", event => {
+  event.waitUntil(
+    caches.keys().then(keys =>
+      Promise.all(
+        keys
+          .filter(key => key !== CACHE_NAME)
+          .map(key => caches.delete(key))
+      )
+    )
+  );
+  self.clients.claim();
+});
+
+self.addEventListener("fetch", event => {
+  if (event.request.method !== "GET") return;
+
+  event.respondWith(
+    fetch(event.request)
+      .then(response => {
+        const copy = response.clone();
+        caches.open(CACHE_NAME).then(cache => cache.put(event.request, copy));
+        return response;
+      })
+      .catch(() => caches.match(event.request))
+  );
+});
